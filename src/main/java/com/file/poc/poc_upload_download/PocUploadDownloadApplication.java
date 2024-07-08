17a5c3697c665b4615881cdb6201d05d9b3a28a7
Vivek <agraharivivek13@gmail.com>
2024-07-08 13:19:53 +0530

    Initial Commit

---
+++ src/main/java/com/file/poc/poc_upload_download/PocUploadDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.file.poc.poc_upload_download;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocUploadDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocUploadDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/file/poc/poc_upload_download/PocUploadDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.file.poc.poc_upload_download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocUploadDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
